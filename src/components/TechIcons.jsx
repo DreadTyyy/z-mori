44f0eb2db3bd69435d60dab27a5706022fce312d
adib <adibhaidarzaky@gmail.com>
2024-07-19 18:25:34 +0700

    add: icon express

--- src/components/TechIcons.jsx
+++ src/components/TechIcons.jsx
@@ -5,14 +5,20 @@ import {
   FaBootstrap,
   FaPython,
 } from "react-icons/fa";
-import { SiMysql, SiJavascript, SiPhp, SiChakraui } from "react-icons/si";
+import {
+  SiExpress,
+  SiMysql,
+  SiJavascript,
+  SiPhp,
+  SiChakraui,
+} from "react-icons/si";
 import { RiTailwindCssFill } from "react-icons/ri";
 import { PropTypes } from "prop-types";
 import { Text } from "@chakra-ui/react";
 
 const TechIcons = ({ icon, customColor }) => {
   const iconMap = {
-    reactjs: { name: FaReact, color: "#23CDEB" },
+    react: { name: FaReact, color: "#23CDEB" },
     html: { name: FaHtml5, color: "#E44F26" },
     css: { name: FaCss3Alt, color: "#1572B6" },
     bootstrap: { name: FaBootstrap, color: "#6610F2" },
@@ -22,6 +28,7 @@ const TechIcons = ({ icon, customColor }) => {
     chakraui: { name: SiChakraui, color: "#38BDF8" },
     tailwind: { name: RiTailwindCssFill, color: "#38BDF8" },
     mysql: { name: SiMysql, color: "#ffffff", size: "28px" },
+    express: { name: SiExpress, color: "#ffffff" },
   };
 
   const iconData = iconMap[icon.toLowerCase()];
